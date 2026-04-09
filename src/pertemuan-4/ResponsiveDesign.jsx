ca8513e9892cc7b463c4a80c01eda4359196c883
amelia24si-sudo <amelia24si@mahasiswa.pcr.ac.id>
2026-04-09 16:05:15 +0700

    Pertemuan-4, part 2

--- src/pertemuan-4/ResponsiveDesign.jsx
+++ src/pertemuan-4/ResponsiveDesign.jsx
@@ -1,9 +1,13 @@
-function ResponsiveText(){
+export default function ResponsiveText(){
     return (
+        <>
         <p className="text-sm md:text-lg lg:text-xl xl:text-2xl mb-4">
             Coba lakukan zoom in atau zoom out. Perhatikan bahwa ukuran teks akan menyesuaikan dengan ukuran layar.<br/>
             Coba hapus class yang menggunakan prefix breakpoint (md:xxx, lg:xxx, xl:xxx) dan lihat perbedaannya!
         </p>
+        <ResponsiveWidth/>
+        <ResponsiveLayout/>
+        </>
     )
 }
 function ResponsiveWidth(){
